e2c4d2d690e27fdbbd7bcded575e107b79c1aec4
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-31 22:49:14 +0300

    Implement multi-number multiplication function

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@
   function add(...numbers) {
     return numbers.reduce((sum, number) => sum + number, 0);
   }
+
+  function multiply(...numbers) {
+    return numbers.reduce((product, number) => product * number, 1);
+  }
 })();
