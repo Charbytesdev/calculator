d35e73a12910dce483fbd2e9e355f68aab9aae09
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-31 23:25:54 +0300

    Implement binary arithmetic operator function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 (function () {
+  let firstNumber = NaN;
+  let secondNumber = NaN;
+  let operator = null;
+
   function add(...numbers) {
     return numbers.reduce((sum, number) => sum + number, 0);
   }
@@ -13,5 +17,18 @@
 
   function divide(...numbers) {
     return numbers.reduce((quotient, number) => quotient / number);
+  }
+
+  function operate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+      case "+":
+        return add(firstNumber, secondNumber);
+      case "-":
+        return subtract(firstNumber, secondNumber);
+      case "*":
+        return multiply(firstNumber, secondNumber);
+      case "/":
+        return divide(firstNumber, secondNumber);
+    }
   }
 })();
