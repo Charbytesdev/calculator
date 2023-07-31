3da18590b6156cef495e8124e89ecf536cdb4bf9
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-07-31 23:12:40 +0300

    Implement multi-number subtraction function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
     return numbers.reduce((sum, number) => sum + number, 0);
   }
 
+  function subtract(...numbers) {
+    return numbers.reduce((difference, number) => difference - number);
+  }
+
   function multiply(...numbers) {
     return numbers.reduce((product, number) => product * number, 1);
   }
